8d226ce1eda128e77dd0fdf987eb39ec6313b478
luangelsdorf <luanferreira2136@gmail.com>
2022-10-25 14:09:57 -0300

    Responsiveness meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import 'src/styles/styles.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
