548a00d114d97b6180df06bff37cd58817aa0220
luangelsdorf <luanferreira2136@gmail.com>
2022-10-28 15:32:44 -0300

    Title adjustments

--- src/components/common/Title/index.js
+++ src/components/common/Title/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Title.module.scss';
 
-export default function Title({ title, overline, align = 'center', spacing = 24, dark, children }) {
+export default function Title({ title, overline, align = 'center', spacing = 24, variant, children }) {
   return (
     <header
-      className={`${styles.title} ${dark ? styles.dark : ''}`}
+      className={`${styles.title} ${styles[variant]}`}
       style={{
         gap: `${spacing}px`,
         alignItems: align,
