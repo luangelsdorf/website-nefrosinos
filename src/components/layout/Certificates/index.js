9d23eeb21d9527423b7422ba557cf1b2578fe84f
luangelsdorf <luanferreira2136@gmail.com>
2022-11-01 14:51:36 -0300

    Certificates section

---
+++ src/components/layout/Certificates/index.js
@@ -0,0 +1,30 @@
+import Image from 'next/future/image';
+import React from 'react';
+import { apiURL } from 'src/utils/env';
+import styles from './Certificates.module.scss';
+
+export default function Certificates({ content }) {
+  return (
+    <div className="container">
+      <div className={styles.section}>
+        <div className={styles.text}>
+          <h3>{content.title}</h3>
+          <p>{content.text}</p>
+        </div>
+        <div className={styles.certificates}>
+          {
+            content.certificates.map(cert => (
+              <Image
+                src={apiURL + cert.image.data.attributes.url}
+                width={cert.image.data.attributes.width}
+                height={cert.image.data.attributes.height}
+                alt="Certificado"
+                key={cert.id}
+              />
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
