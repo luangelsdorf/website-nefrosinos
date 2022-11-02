e032a5211f0423ee58366eb320f099e8c606ec5b
luangelsdorf <luanferreira2136@gmail.com>
2022-11-02 10:04:27 -0300

    Copyright changes

--- src/components/layout/Copyright/index.js
+++ src/components/layout/Copyright/index.js
@@ -7,6 +7,17 @@ export default function Copyright({ content }) {
       <div className="container">
         <div className={styles.content}>
           <small>{content.copyright}</small>
+          <div className={styles.icons}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" height="32" width="32">
+              <rect width="100" height="100" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" height="32" width="32">
+              <rect width="100" height="100" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" height="32" width="32">
+              <rect width="100" height="100" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
