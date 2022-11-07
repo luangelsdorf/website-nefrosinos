9c1bae143766751f717a81e5cb2649697b122b34
luangelsdorf <luanferreira2136@gmail.com>
2022-11-07 08:57:22 -0300

    AboutUs section

---
+++ src/components/about/AboutUs/index.js
@@ -0,0 +1,37 @@
+import Image from 'next/future/image';
+import Link from 'next/link';
+import React from 'react';
+import Title from 'src/components/common/Title';
+import { apiURL } from 'src/utils/env';
+import styles from './AboutUs.module.scss';
+
+export default function AboutUs({ content }) {
+  return (
+    <div className={styles.section}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-5">
+            <div className={styles.text}>
+              <Title title={content.title.title} overline={content.title.overline} align="flex-start" />
+              <p>{content.text}</p>
+              <Link href="/contato">
+                <a className="btn icon">Fale Conosco</a>
+              </Link>
+            </div>
+          </div>
+          <div className="col-12 col-lg-6 offset-lg-1">
+            <div className={styles.image}>
+              <Image
+                src={apiURL + content.image.data.attributes.url}
+                /* width={content.image.data.attributes.width}
+                height={content.image.data.attributes.height} */
+                fill
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
