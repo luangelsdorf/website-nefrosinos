dfc301cfedce5331928b5c2707c7d4d6f98f372b
luangelsdorf <luanferreira2136@gmail.com>
2022-11-09 11:43:28 -0300

    Applied TeamCarousel on homepage

--- src/components/home/OurTeam/index.js
+++ src/components/home/OurTeam/index.js
@@ -1,39 +1,12 @@
 import React from 'react';
-import MemberCard from 'src/components/common/MemberCard';
-import Title from 'src/components/common/Title';
-import { Autoplay, FreeMode } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import TeamCarousel from 'src/components/common/TeamCarousel';
 import styles from './OurTeam.module.scss';
 
 export default function OurTeam({ content, team }) {
   return (
     <div className={styles.section}>
       <Title title={content.title.title} overline={content.title.overline} variant="dark" />
-      <div className="container">
-        <Swiper
-          spaceBetween={24}
-          slidesPerView="auto"
-          modules={[Autoplay, FreeMode]}
-          loop
-          speed={1800}
-          freeMode
-          autoplay={{
-            delay: 200,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true,
-          }}
-        >
-          {
-            team.team.map(member => (
-              <SwiperSlide>
-                <div>
-                  <MemberCard photo={member.photo} name={member.name} speciality={member.speciality} />
-                </div>
-              </SwiperSlide>
-            ))
-          }
-        </Swiper>
-      </div>
+      <TeamCarousel team={team} />
     </div>
   )
 }
