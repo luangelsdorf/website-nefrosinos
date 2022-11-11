fcee721c25e9f542a413d1264018463854958c4a
luangelsdorf <luanferreira2136@gmail.com>
2022-11-11 09:03:59 -0300

    About Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,49 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
-import fetchAPI from 'src/utils/fetch';
-import Section from 'src/components/common/Section';
-import Title from 'src/components/common/Title';
-import Image from 'next/future/image';
-import Collage from 'src/components/common/Collage';
-import HeroSlider from 'src/components/home/HeroSlider';
-import Service from 'src/components/home/Service';
-import AboutUs from 'src/components/home/AboutUs';
-import OurTeam from 'src/components/home/OurTeam';
-import Partners from 'src/components/home/Partners';
-import Structure from 'src/components/home/Structure';
-import Contact from 'src/components/layout/Contact';
+import fetchAPI, { getLayoutContent } from 'src/utils/fetch';
+import MainLayout from 'src/components/layout/Main';
 
-export default function Home({ home, partners, team, footer, info }) {
+export default function Home({ home, partners, team }) {
 
   return (
-    <>
-      <main>
-        {/* <HeroSlider content={home.bannerSlider} height={880} /> */}
-        {/* <Section>
+    <main>
+      <h1 style={{ height: '300vh' }}>Homepage</h1>
+      {/* <Header /> */}
+      {/* <HeroSlider content={home.bannerSlider} height={880} /> */}
+      {/* <Section>
           <Service content={home.service} />
         </Section> */}
-        {/* <Section>
+      {/* <Section>
           <AboutUs content={home.aboutUs} />
         </Section> */}
-        {/* <Section pt="96" >
+      {/* <Section pt="96" >
           <OurTeam content={home.ourTeam} team={team} />
         </Section> */}
-        {/* <Section>
+      {/* <Section>
           <Partners content={home.partners} partners={partners} length={6} />
         </Section> */}
-        {/* <Section>
+      {/* <Section>
           <Structure content={home.structure} />
         </Section> */}
-        <Section pb="108">
-          <Contact content={footer} info={info} />
-        </Section>
-      </main>
-    </>
+    </main>
   )
 }
+
+Home.layout = MainLayout;
 
 export async function getStaticProps() {
   const home = await fetchAPI('home-site');
   const partners = await fetchAPI('parceiro');
   const team = await fetchAPI('equipe');
-  const footer = await fetchAPI('rodape');
-  const info = await fetchAPI('info');
+
+  const layout = await getLayoutContent();
 
   return {
     props: {
       home,
       partners,
       team,
-      footer,
-      info,
+
+      layout, // will only be used in `_app.js`
     }
   }
 }
---
+++ src/pages/sobre-nos.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import AboutOurTeam from 'src/components/about/AboutOurTeam';
+import AboutUs from 'src/components/about/AboutUs';
+import Hero from 'src/components/about/Hero';
+import OurTeam from 'src/components/about/OurTeam';
+import Purpose from 'src/components/about/Purpose';
+import Timeline from 'src/components/about/Timeline';
+import Section from 'src/components/common/Section';
+import TeamCarousel from 'src/components/common/TeamCarousel';
+import MainLayout from 'src/components/layout/Main'
+import fetchAPI, { getLayoutContent } from 'src/utils/fetch';
+
+export default function SobreNos({ about, team }) {
+  return (
+    <main>
+      <h1 style={{ height: '300vh' }}>About Page</h1>
+      {/* <Section>
+        <AboutUs content={about.aboutUs} />
+      </Section> */}
+      {/* <Section>
+        <Purpose content={about.purpose} />
+      </Section> */}
+      {/* <Section pt="147">
+        <Timeline content={about.timeline} />
+      </Section> */}
+      {/* <Section>
+        <OurTeam content={about.ourTeam} team={team} />
+      </Section> */}
+      {/* <Section>
+        <AboutOurTeam content={about.aboutOurTeam} />
+      </Section> */}
+    </main>
+  )
+}
+
+SobreNos.layout = MainLayout;
+
+export async function getStaticProps() {
+  const about = await fetchAPI('sobre-nos');
+  const team = await fetchAPI('equipe');
+
+  const layout = await getLayoutContent();
+
+  return {
+    props: {
+      about,
+      team,
+
+      layout, // will only be used in `_app.js`
+    }
+  }
+}
