776caaa9be70616a4e52c09fa92d103d89b735c9
luangelsdorf <luanferreira2136@gmail.com>
2022-11-13 13:29:21 -0300

    Peritoneal Dialysis section

---
+++ src/components/services/PeritonealDialysis/index.js
@@ -0,0 +1,37 @@
+import Image from 'next/future/image';
+import React from 'react';
+import Title from 'src/components/common/Title';
+import { apiURL } from 'src/utils/env';
+import styles from './PeritonealDialysis.module.scss';
+
+export default function PeritonealDialysis({ content }) {
+  return (
+    <div className={styles.bgWrapper}>
+      <div className="container">
+        <div className={styles.section}>
+          <div className={styles.textContent}>
+            <Title title={content.title.title} overline={content.title.overline} align="flex-start" />
+            <p>{content.text}</p>
+            <strong>
+              <img src={apiURL + content.warning.icon.data.attributes.url} width="56" height="56" />
+              <span>{content.warning.text}</span>
+            </strong>
+          </div>
+
+          <Image src={apiURL + content.image.data.attributes.url} width="480" height="520" alt="" />
+
+          <div className={styles.advantages}>
+            {
+              content.highlights.map(item => (
+                <article key={item.id}>
+                  <h3>{item.title}</h3>
+                  <p>{item.text}</p>
+                </article>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
