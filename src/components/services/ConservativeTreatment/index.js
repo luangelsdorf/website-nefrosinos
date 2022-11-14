257765b19a805ee0ca3c17196496c91b032b7bd9
luangelsdorf <luanferreira2136@gmail.com>
2022-11-14 09:15:36 -0300

    Conservative Tratment section

---
+++ src/components/services/ConservativeTreatment/index.js
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import React from 'react';
+import Title from 'src/components/common/Title';
+import styles from './ConservativeTreatment.module.scss';
+
+export default function ConservativeTreatment({ content }) {
+  return (
+    <div className={styles.section}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-lg-8 mx-auto">
+            <div className={styles.textContent}>
+              <Title title={content.title.title} overline={content.title.overline} align="center" variant="light" />
+              <p>{content.text}</p>
+              <div className={styles.links}>
+                <Link href="/">
+                  <a className="btn secondary">Conheça Nossa Equipe</a>
+                </Link>
+                <Link href="/">
+                  <a className="btn icon">Fale Conosco</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
