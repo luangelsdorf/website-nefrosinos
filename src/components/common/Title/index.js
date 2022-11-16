dc45b54c1dad6df20a099ba7afa237bc07e00de5
luangelsdorf <luanferreira2136@gmail.com>
2022-11-16 08:36:28 -0300

    Improved Title'a `align` prop

--- src/components/common/Title/index.js
+++ src/components/common/Title/index.js
@@ -4,12 +4,23 @@ import styles from './Title.module.scss';
 export default function Title({ content, align = 'center', spacing = 24, variant, children }) {
   const { title, overline } = content;
 
+  let alignment = { flex: align, text: align };
+  switch (align) {
+    case 'start':
+      alignment.flex = 'flex-start';
+      alignment.text = 'left';
+    case 'end':
+      alignment.flex = 'flex-end';
+      alignment.text = 'right';
+  }
+
   return (
     <header
       className={`${styles.title} ${styles[variant]}`}
       style={{
         gap: `${spacing}px`,
-        alignItems: align,
+        alignItems: alignment.flex,
+        textAlign: alignment.text,
       }}
     >
       {
