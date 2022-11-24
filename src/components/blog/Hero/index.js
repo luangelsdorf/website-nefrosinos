b30cea1ec6f53336cb74c55db780a76247c3e23e
luangelsdorf <luanferreira2136@gmail.com>
2022-11-24 15:00:38 -0300

    Adjusted container alignment

--- src/components/blog/Hero/index.js
+++ src/components/blog/Hero/index.js
@@ -8,17 +8,21 @@ export default function Hero({ content, categories }) {
   return (
     <section className={styles.section}>
       <Banner cover={apiURL + content.cover.data.attributes.url}>
-        <div className={styles.bannerContent}>
-          <h1>{content.title}</h1>
-          <h2>{content.text}</h2>
-          <div className={styles.categories}>
-            {
-              categories.map(cat => (
-                <Link href="/categorias" key={cat.id}>
-                  <a className="btn small">{cat.attributes.name}</a>
-                </Link>
-              ))
-            }
+        <div className="row">
+          <div className="col-12 col-xl-10 mx-auto">
+            <div className={styles.bannerContent}>
+              <h1>{content.title}</h1>
+              <h2>{content.text}</h2>
+              <div className={styles.categories}>
+                {
+                  categories.map(cat => (
+                    <Link href="/categorias" key={cat.id}>
+                      <a className="btn small">{cat.attributes.name}</a>
+                    </Link>
+                  ))
+                }
+              </div>
+            </div>
           </div>
         </div>
       </Banner>
--- src/components/blog/MainPosts/index.js
+++ src/components/blog/MainPosts/index.js
@@ -5,18 +5,12 @@ import styles from './MainPosts.module.scss';
 export default function MainPosts({ posts }) {
   return (
     <div className={styles.section}>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-xl-10 mx-auto">
-            <div className="row" style={{ rowGap: '80px' }}>
-              {
-                posts.map((post, index) => (
-                  <PostCard key={post.id} {...post.attributes} nthChild={index} />
-                ))
-              }
-            </div>
-          </div>
-        </div>
+      <div className="row" style={{ rowGap: '80px' }}>
+        {
+          posts.map((post, index) => (
+            <PostCard key={post.id} {...post.attributes} nthChild={index} />
+          ))
+        }
       </div>
     </div>
   )
