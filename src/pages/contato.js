e49a2ebcf565555a21f35525a83c55693ff6afc4
luangelsdorf <luanferreira2136@gmail.com>
2022-11-29 22:42:47 -0300

    Contact page styles

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -4,23 +4,26 @@ import Section from 'src/components/common/Section';
 import Address from 'src/components/contact/Address';
 import ContactInfo from 'src/components/contact/ContactInfo';
 import Hero from 'src/components/contact/Hero';
+import Hours from 'src/components/contact/Hours';
 import MainLayout from 'src/components/layout/Main';
 import fetchAPI, { getLayoutContent } from 'src/utils/fetch';
 
 export default function Contato({ contact, faq, layout: { info } }) {
   return (
     <main>
-      <h1 style={{ height: '300vh' }}>Contact Page</h1>
-      {/* <Hero info={info} /> */}
-      {/* <Section>
+      <Hero info={info} />
+      <Section mt="-100" className="bg-gray">
         <Address content={contact.location} info={info} />
-      </Section> */}
-      {/* <Section pb="200" mb="160" className="bg-gray">
+      </Section>
+      <Section pt="120" pb="200" className="bg-gray">
         <ContactInfo content={contact.details} info={info} />
-      </Section> */}
-      {/* <Section>
+      </Section>
+      <Section mt="-75" mb="160">
+        <Hours info={info} />
+      </Section>
+      <Section mb="280">
         <FAQ content={faq} />
-      </Section> */}
+      </Section>
     </main>
   )
 }
