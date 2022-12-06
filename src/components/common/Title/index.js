a199dffbee0d572b5787bea3ad94098cacc00d85
luangelsdorf <luanferreira2136@gmail.com>
2022-12-06 09:04:55 -0300

    Title component improvement

--- src/components/common/Title/index.js
+++ src/components/common/Title/index.js
@@ -4,27 +4,8 @@ import styles from './Title.module.scss';
 export default function Title({ content, align = 'center', spacing = 24, variant, children }) {
   const { title, overline } = content;
 
-  let alignment = { flex: align, text: align };
-  switch (align) {
-    case 'start':
-      alignment.flex = 'flex-start';
-      alignment.text = 'left';
-      break;
-    case 'end':
-      alignment.flex = 'flex-end';
-      alignment.text = 'right';
-      break;
-  }
-
   return (
-    <header
-      className={`${styles.title} ${styles[variant]}`}
-      style={{
-        gap: `${spacing}px`,
-        alignItems: alignment.flex,
-        textAlign: alignment.text,
-      }}
-    >
+    <header className={`${styles.title} ${styles[variant] ?? ''} ${styles[align] ?? ''}`} style={{ gap: `${spacing}px` }}>
       {
         children ? (
           children
