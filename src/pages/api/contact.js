fc251a68287a1286adecc33aa226e2e6caea562a
luangelsdorf <luanferreira2136@gmail.com>
2022-12-15 21:51:18 -0300

    Contact API setup

---
+++ src/pages/api/contact.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = {
+      from: process.env.SENDER_EMAIL,
+      to: 'luanferreira2136@gmail.com',
+      subject: `Nova Mensagem | Website NefroSinos`,
+      text: `Mensagem: ${req.body.message} | Enviada de ${req.body.email}`,
+      html: `
+                <div>Você recebeu uma nova mensagem no seu site. Confira as informações abaixo:</div>
+                <p><strong>Nome</strong>: ${req.body.name}</p>
+                <p><strong>Telefone</strong>: ${req.body.phone}</p>
+                <p><strong>E-mail</strong>: ${req.body.email}</p>
+                <p><strong>Mensagem</strong>: ${req.body.message}</p>
+              `
+    }
+
+    await sgMail.send(message);
+    return res.status(200).end();
+  }
+
+  return res.status(405).end();
+}
